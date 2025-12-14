c843f12ec47161815019ae87300d8984aaf0f7bc
heke-maker <kt.wkn3a@gmail.com>
2025-12-14 23:13:20 +0900

    Fix all deployment and asset errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root 'tweets#index'
   
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pa#manifest", as: :pwa_manifest
-
-  root 'tweets#index'
 
   get 'tweets/:tweet_id/likes' => 'likes#create'
   get 'tweets/:tweet_id/likes/:id' => 'likes#destroy'
