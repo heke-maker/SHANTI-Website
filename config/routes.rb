fe208cf7be181fb31b1d652e0cc0de4829a0d814
heke-maker <kt.wkn3a@gmail.com>
2025-12-15 04:31:50 +0900

    FIX: Critical routing typo (pa to pwa) and clean up redundant likes routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,6 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pa#manifest", as: :pwa_manifest
 
-  get 'tweets/:tweet_id/likes' => 'likes#create'
-  get 'tweets/:tweet_id/likes/:id' => 'likes#destroy'
-
   resources :blogs
   resources :users
 
